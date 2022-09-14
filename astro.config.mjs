98ee18a56ac57a0583fb9b08adacdf4b0b177e7c
halcyon <55317573+hxlcyxn@users.noreply.github.com>
2022-09-14 11:30:10 +0200

    style: format with prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   integrations: [sitemap()],
-  site: "https://hxlcyxn.github.io"
+  site: "https://hxlcyxn.github.io",
 });

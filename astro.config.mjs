73ddb4dc8887b196b6c52d99ffc330d7b0353957
halcyon <55317573+hxlcyxn@users.noreply.github.com>
2023-10-18 22:38:29 +0200

    wipe ☄

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "astro/config";
-
-import sitemap from "@astrojs/sitemap";
-
-// https://astro.build/config
-export default defineConfig({
-  integrations: [sitemap()],
-  site: "https://hxlcyxn.github.io",
-});
